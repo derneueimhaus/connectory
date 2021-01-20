dafab2bcbd85b0c02ec74ec9e08029a6e2dbe26f
crevulus <crevulus@gmail.com>
2021-01-20 12:54:44 +0100

    finished projects view

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 
-export default class ProjectCard extends Component {
-  constructor(props) {
-    super(props);
-  }
+import "../styles/ProjectCard.css";
 
+export default class ProjectCard extends Component {
   render() {
     return (
-      <div>
-        <p>This is a project card</p>
+      <div
+        className="project-card"
+        style={{ backgroundImage: `url(${this.props.data.photo})` }}
+      >
+        <h4>{this.props.data.headline}</h4>
+        <h5>{this.props.data.subheader}</h5>
       </div>
     );
   }
